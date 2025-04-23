a8f91df49b27ddc6162ecd0ecd3e09a4692cc5d6
vu <vupot123@gmail.com>
2025-04-23 17:11:40 +0700

    thay doi khung BE

---
+++ src/main/java/com/example/movie_streaming/demoService/src/main/java/com/example/movie_streaming/demoService/kafka/KafkaConsumerService.java
@@ -0,0 +1,15 @@
+package com.example.movie_streaming.demoService.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "test-topic", groupId = "my-group")
+    public void listen(ConsumerRecord<String, String> record) {
+        System.out.println(" Received from Kafka: " + record.value());
+    }
+}
+
