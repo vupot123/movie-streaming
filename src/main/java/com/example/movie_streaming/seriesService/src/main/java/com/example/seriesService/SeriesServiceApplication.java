470a9e4fa7e404a53d87acb8e173fcb351d3436c
vu <vupot123@gmail.com>
2025-04-24 13:44:49 +0700

    das

---
+++ src/main/java/com/example/movie_streaming/seriesService/src/main/java/com/example/seriesService/SeriesServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.seriesService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeriesServiceApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/movie_streaming/streamService/src/main/java/com/example/streamService/StreamServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.streamService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamServiceApplication.class, args);
+    }
+}
