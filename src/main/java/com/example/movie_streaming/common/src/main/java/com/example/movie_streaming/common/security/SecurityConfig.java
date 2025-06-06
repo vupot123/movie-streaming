cf1260582f0541f37e7d2a747c0399519e2ef5bb
uzipremium <dinhv7583@gmail.com>
2025-06-07 00:33:00 +0700

    a

--- src/main/java/com/example/movie_streaming/common/src/main/java/com/example/movie_streaming/common/security/SecurityConfig.java
+++ src/main/java/com/example/movie_streaming/common/src/main/java/com/example/movie_streaming/common/security/SecurityConfig.java
@@ -45,8 +45,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-                "http://localhost:4200",
-                "http://localhost:3000",
+                "http://localhost:5173",
                 "https://yourdomain.web.app",
                 "https://yourdomain.com"
         ));
